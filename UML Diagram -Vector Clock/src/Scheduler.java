da79a5647a54610775f6e6cb80818d98232ba085
svishrut93 <s.vishrut93@gmail.com>
2017-10-05 18:43:13 -0700

    Update Scheduler.java

--- UML Diagram -Vector Clock/src/Scheduler.java
+++ UML Diagram -Vector Clock/src/Scheduler.java
@@ -1,113 +1,52 @@
-import java.util.concurrent.TimeUnit;
 
-public class Scheduler {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Schedular implements Runnable{
+
+	String fileName;
+	List<Processor> processors;
+	Processor P;
 	
-	public static void main(String[] args)
-	{
-		Processor P0 = new Processor(0,3);
-    	Processor P1 = new Processor(1,3);
-    	Processor P2 = new Processor(2,3);
-    	
-    	System.out.println("Main thread starts....");
-    	
-    	
-    	int id0 = 0 ; 
-    	int id1=1; 
-    	int id2= 2; 
-    	
-    	Thread t0 = new Thread(new tasker(P0,P1,P2,id0));
-    	Thread t1 = new Thread(new tasker(P0,P1,P2,id1));
-    	Thread t2 = new Thread(new tasker(P0,P1,P2,id2));
-    	t0.start();
-    	t1.start();
-    	t2.start();
-
-	}
-}
-
-
-class tasker implements Runnable{
-	
-	private Processor p1;
-	private Processor p2;
-	private Processor p3;
-	private int id ; 
-	
-	public void run()
-	{
-		System.out.println("Sched run method");
-		if(this.id==0)
-		{
-			this.executionPlanForP0(p1, p2, p3);
-		}
-		else if (this.id==1)
-		{
-			this.executionPlanForP1(p1, p2, p3);
-		}
-		else
-		{
-			this.executionPlanForP2(p1, p2, p3);
-		}
-				
-		
+	public Schedular(String fn, Processor P, List<Processor> processors){
+		this.fileName = fn;
+		this.P = P;
+		this.processors = processors;
 	}
 	
-	public tasker (Processor A, Processor B, Processor C, int id)
-	{
-		this.p1=A;
-		this.p2=B;
-		this.p3=C; 
-		this.id = id ; 
-		System.out.println("Sched method");
-		
-	}
+	public void run(){
+		String line = "";
+		 try {
+	            // FileReader reads text files in the default encoding.
+	            FileReader fileReader = new FileReader(fileName);
 	
-	public void executionPlanForP0(Processor A, Processor B, Processor C)
-	{
-		
-		System.out.println("State of vc b4 execution");
-		for(int i = 0 ; i < 3 ; i ++)
-		{
-			System.out.println(A.vc.vc[i]);
-		}
-		
-		
-		A.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, A.vc) , A )  ; 
-		A.sendMessgeToProcessor(new Message(MessageType.SEND, A.vc),   B  );
-		// A.sendMessgeToProcessor(new Message(MessageType.SEND,A.vc),C);
-		 //A.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, A.vc) , A )  ; 
-		 //Receives
-		 //A.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, A.vc) , A )  ; 
-		
-		 System.out.println("this is plan of P0 ");
-
-		 
-	}
+	            // Always wrap FileReader in BufferedReader.
+	            BufferedReader bufferedReader = new BufferedReader(fileReader);
 	
-
-	public void executionPlanForP1(Processor A, Processor B, Processor C) 
-	{
-		//recieve event 
-		//receive event 
-		//B.sendMessgeToProcessor (new Message(MessageType.COMPUTATION, B.vc) , C );
-		//receieve event
-		 System.out.println("this is plan of P1");
-		
+	            while((line = bufferedReader.readLine()) != null) {
+//	            	String threadName = Thread.currentThread().getName();
+//		            System.out.println("Thread Name = "+ threadName);
+		            if (Objects.equals(line, "C")){
+	            		P.sendMessgeToProcessor(MessageType.COMPUTATION, null);
+	            	}
+		            if(Objects.equals(line.charAt(0), 'S')){
+		            	int procId = Character.getNumericValue(line.charAt(line.length()-1));
+		            	Processor receiver = processors.get(procId);
+		            	P.sendMessgeToProcessor(MessageType.SEND, receiver);
+		            }
+	            }   
+	            // Always close files.
+	            bufferedReader.close();         
+	        }
+	        catch(FileNotFoundException ex) {
+	            System.out.println("Unable to open file '" + fileName + "'");                
+	        }
+	        catch(IOException ex) {
+	            System.out.println("Error reading file '" + fileName + "'");                  
+	        }
 	}
-	public void executionPlanForP2(Processor A, Processor B, Processor C) 
-	{
-		
-		//C.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, C.vc) , C )  ; 
-		//C.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, C.vc) , C )  ; 
-		//C.sendMessgeToProcessor(  new Message(MessageType.SEND, C.vc) , B )  ;
-		//recieve
-		//C.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, C.vc) , A )  ; 
-		//recieve
-		//C.sendMessgeToProcessor(  new Message(MessageType.COMPUTATION, C.vc) , C )  ; 
-		
-		System.out.println("this is plan of P2 ");
-		
-	}
-	
 	
 }
